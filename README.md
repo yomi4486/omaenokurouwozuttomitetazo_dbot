7bcfda063c1ab28bba186c69698c63db7525bb05
yomi4486 <dovahkiin4486@icloud.com>
2024-12-15 23:15:08 +0900

    いいフォントみつけた

---
+++ README.md
@@ -0,0 +1,3 @@
+# お前の苦労をずっと見ていたぞBot
+
+フォント：https://j-font.com/material/font
--- create_img.py
+++ create_img.py
@@ -1,4 +1,12 @@
 from PIL import Image,ImageDraw,ImageFont
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os 
+
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 def image_process(base_text:str):
     if len(base_text) > 30:
         return 1
@@ -6,7 +14,7 @@ def image_process(base_text:str):
         base_text = f"{base_text[:14]}\n{base_text[14:]}"
 
     img = Image.open('main.png')
-    font = ImageFont.truetype('LightNovelPOPv2.otf', 40)
+    font = ImageFont.truetype(os.environ.get("FONT_PATH"), 40)
     draw = ImageDraw.Draw(img)
 
     #左上の点のx座標, 左上の点のy座標, 右下の点のx座標, 右下の点のy座標
@@ -20,3 +28,8 @@ def image_process(base_text:str):
     draw.text((431-x, 230), base_text, 'black',font=font,align='center')
     img.save("result.jpg")
     return 0
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) >=2:
+        image_process(sys.argv[1])
